a86f4eefbd349bebbce46cd261db73668432abbb
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2022-05-23 18:02:15 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,6 @@
+const myArray = ['I', 'love', 'chocolate', 'frogs'];
+const madeAString = myArray.join(' ');
+console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
